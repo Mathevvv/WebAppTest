aa07ddf5614eeb1a36321e100ef8b03f97612ba1
matve <matvey.karasev.00@gmail.com>
2025-04-11 15:59:26 +0300

    Второй коммит

--- src/main/java/org/example/rest/entity/Borrower.java
+++ src/main/java/org/example/rest/entity/Borrower.java
@@ -1,23 +1,23 @@
 package org.example.rest.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Entity;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import org.hibernate.annotations.Entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-@Entity
-@Table(name="borrowers")
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+//@Entity
+//@Table(name="borrowers")
 public class Borrower {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer borrower_id;
     private String name;
     private String email;
